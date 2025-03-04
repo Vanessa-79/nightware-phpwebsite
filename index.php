d7e1400d094a381d270e0616c83748b2ba484d29
Vanessa <nassangavanessa2@gmail.com>
2025-03-04 12:06:17 +0300

    created the views directory

--- index.php
+++ index.php
@@ -1,30 +1,4 @@
 <?php
 
-// Sample data for books
-// $books = [
-//     [
-//         "name" => "Do Androids Dream of Electric Sheep?",
-//         "releaseYear" => 1968,
-//         "author" => "Philip K. Dick",
-//         "purchaseUrl" => "#"
-//     ],
-//     [
-//         "name" => "The Langoliers",
-//         "releaseYear" => 1990,
-//         "author" => "Stephen King",
-//         "purchaseUrl" => "#"
-//     ],
-//     [
-//         "name" => "Uganda Rising",
-//         "releaseYear" => 2021,
-//         "author" => "John Doe",
-//         "purchaseUrl" => "#"
-//     ],
-// ];
-
-// // Filtering books (you can modify as needed)
-// $filteredBooks = $books;
-
-// Include the main view file
-require 'index.view.php';
+require 'views/index.view.php';
 ?>
