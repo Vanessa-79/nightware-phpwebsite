d4191ba795331530bfbd54a061cc57612a6d12bb
Vanessa <nassangavanessa2@gmail.com>
2025-03-06 21:32:57 +0300

    created controllers and php routes

--- index.php
+++ index.php
@@ -2,7 +2,37 @@
 
 require 'functions.php';
 
-$heading = 'Home';
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+// if ($uri === '/') {
+//     require 'controllers/index.php';
+// } else if ($uri === '/about') {
+//     require 'controllers/about.php';
+// } else if ($uri === '/contact') {
+//     require 'controllers/contact.php';
+// }
+
+// associative array
+$routes = [
+
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+
+];
+
+function abort($code)
+{
+    http_response_code($code);
 
 
-require 'views/index.view.php';
+    require 'views/{$code}.php';
+
+    die();
+}
+
+if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+} else {
+    abort(404);
+}
---
+++ views/404.php
@@ -0,0 +1,24 @@
+<?php require('partials/head.php') ?>
+
+<!-- nav -->
+
+<?php require('partials/nav.php') ?>
+
+<!-- header -->
+<?php require('partials/banner.php') ?>
+
+<!-- main -->
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <!-- Your content -->
+        <h1 class="text-2x1 font-bold">Sorry page not found</h1>
+        <p class="mt-4 text-red-400">
+            <a href="/">Go back home</a>
+        </p>
+    </div>
+</main>
+</div>
+
+</body>
+
+</html>
